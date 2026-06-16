d89920ac7b4f52534ca67fd5f37af5f68dd25807
ismaili <ilyan2smaili@gmail.com>
2026-06-16 10:02:14 +0200

    Presque fin IS

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-LARGEUR_GRILLE = 10
-LONGEUR_GRILLE = 10
+LARGEUR_GRILLE = 15
+LONGEUR_GRILLE = 15
 TAILLE_CASE = 40
 
 LARGEUR_LABYRINTHE = TAILLE_CASE * LARGEUR_GRILLE
@@ -12,16 +12,21 @@ LONGEUR_LABYRINTHE = TAILLE_CASE * LONGEUR_GRILLE
 ECRAN = pygame.display.set_mode((LONGEUR_LABYRINTHE, LARGEUR_LABYRINTHE))
 
 labyrinthe = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, "S", 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-    [1, 1, 1, 0, 0, 0, 0, 0, "E", 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, "S", 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, "E", 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 joueur_x = 40
@@ -33,16 +38,29 @@ while True:
             pygame.quit()
             exit()
 
-        # Détection des touches du clavier
         if event.type == pygame.KEYDOWN:
+            prochain_x = joueur_x
+            prochain_y = joueur_y
+
             if event.key == pygame.K_LEFT:
-                joueur_x -= TAILLE_CASE
+                prochain_x -= TAILLE_CASE
             if event.key == pygame.K_RIGHT:
-                joueur_x += TAILLE_CASE
+                prochain_x += TAILLE_CASE
             if event.key == pygame.K_UP:
-                joueur_y -= TAILLE_CASE
+                prochain_y -= TAILLE_CASE
             if event.key == pygame.K_DOWN:
-                joueur_y += TAILLE_CASE
+                prochain_y += TAILLE_CASE
+
+            case_x = prochain_x // TAILLE_CASE
+            case_y = prochain_y // TAILLE_CASE
+
+            if labyrinthe[case_y][case_x] != 1:
+                joueur_x = prochain_x
+                joueur_y = prochain_y
+
+            if labyrinthe[case_y][case_x] == "E":
+                pygame.quit()
+                exit()
 
     ECRAN.fill((255, 255, 255))
 
@@ -52,10 +70,11 @@ while True:
         for case in ligne:
             if case == 1:
                 pygame.draw.rect(ECRAN, (0, 0, 0), (x, y, TAILLE_CASE, TAILLE_CASE))
+            elif case == "E":
+                pygame.draw.rect(ECRAN, (0, 255, 0), (x, y, TAILLE_CASE, TAILLE_CASE))
             x += TAILLE_CASE
         y += TAILLE_CASE
 
-    # Dessin du joueur (un carré rouge)
     pygame.draw.rect(ECRAN, (255, 0, 0), (joueur_x, joueur_y, TAILLE_CASE, TAILLE_CASE))
 
     pygame.display.flip()
